def09497fad516b17c966e18d77f73cdc85437bb
rev3nt <mikhail.merkulov.2005@mail.ru>
2025-09-24 21:53:05 +0300

    added functions

--- main.py
+++ main.py
@@ -1,31 +1,44 @@
-text = input("Введите текст: ")
+def words_to_dict(words):
+    for word in split_text:
+        if word not in words_counter_dict:
+            words_counter_dict[word] = 1
+        elif word in words_counter_dict:
+            words_counter_dict[word] += 1
+    return words_counter_dict
 
-vowels_counter = 0
 
-sign_list = ['.', ',', '!', '?', ':', ';', '-', '—']
+def vowels_counter(words):
+    vowels = 0
+
+    for word in split_text:
+        for letter in word:
+            if letter in vowels_list:
+                vowels += 1
+    return vowels
+
+def clear_text(text):
+    for s in sign_list:
+        text = text.replace(s, '')
+
+    return text
+
+
+sign_list = ['.', ',', '!', '?', ':', ';', '-', '—', '–', '‐', '(', ')', '[', ']', '{', '}',
+                '"', "'", '`', '«', '»', '“','”', '‘', '’', '…', '/', '<', '>', '&', '*', '%',
+                '#', '@', '$', '^', '~', '|', '+', '=', '_']
 
 vowels_list = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
 
 words_counter_dict = {}
 
-for s in sign_list: # Убираем знаки препинания
-    text = text.replace(s, '')
+text = input("Введите текст: ")
 
-split_text = text.lower().split() # Резделяем слова в массив, снижаем регистр
-
-for word in split_text: # Проход по словам
-    if word not in words_counter_dict: # Добавляем слова в словари и считаем их количество
-        words_counter_dict[word] = 1
-    elif word in words_counter_dict:
-        words_counter_dict[word] += 1
-    for letter in word: # Считаем гласные в каждом слове
-        if letter in vowels_list:
-            vowels_counter += 1
+split_text = str(clear_text(text)).lower().split()
 
 print(f'Количество слов в тексте: {len(split_text)}')
 
 print(f'Самое длинное слово в тексте: {max(split_text, key=len)}')
 
-print(f'Количество гласных в тексте: {vowels_counter}')
+print(f'Количество гласных в тексте: {vowels_counter(split_text)}')
 
-print(f'Слова и частота их повторений в тексте: {words_counter_dict}')
+print(f'Слова и частота их повторений в тексте: {words_to_dict(split_text)}')
