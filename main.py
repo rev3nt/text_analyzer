0d7c09149d52fa370235e507001c3f4bef9c558d
rev3nt <mikhial.merkulov.2005@mail.ru>
2025-09-25 14:13:49 +0300

    Algoritm optimization

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
+import string
+
 def words_to_dict(words):
     for word in split_text:
         if word not in words_counter_dict:
             words_counter_dict[word] = 1
-        elif word in words_counter_dict:
+        else:
             words_counter_dict[word] += 1
+
     return words_counter_dict
 
 
-def vowels_counter(words):
+def vowels_counter(text):
     vowels = 0
 
-    for word in split_text:
-        for letter in word:
-            if letter in vowels_list:
-                vowels += 1
+    for letter in text:
+        if letter in vowels_list:
+            vowels += 1
+
     return vowels
 
 def clear_text(text):
-    for s in sign_list:
-        text = text.replace(s, '')
+    for letter in text:
+        if letter in sign_list:
+            text = text.replace(letter, '')
 
     return text
 
 
-sign_list = ['.', ',', '!', '?', ':', ';', '-', '—', '–', '‐', '(', ')', '[', ']', '{', '}',
-                '"', "'", '`', '«', '»', '“','”', '‘', '’', '…', '/', '<', '>', '&', '*', '%',
-                '#', '@', '$', '^', '~', '|', '+', '=', '_']
+sign_list = string.punctuation
 
 vowels_list = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
 
 words_counter_dict = {}
 
-text = input("Введите текст: ")
+text = input("Введите текст: ").lower()
+#text = "Нет, нет, нет! Это невозможно. Почему? Почему так происходит? Жду, жду, жду ответа...".lower()
 
 split_text = str(clear_text(text)).lower().split()
 
@@ -39,6 +42,6 @@ print(f'Количество слов в тексте: {len(split_text)}')
 
 print(f'Самое длинное слово в тексте: {max(split_text, key=len)}')
 
-print(f'Количество гласных в тексте: {vowels_counter(split_text)}')
+print(f'Количество гласных в тексте: {vowels_counter(text)}')
 
 print(f'Слова и частота их повторений в тексте: {words_to_dict(split_text)}')
